dec7034c941be3f2e9fecb9fca88f9007f126a9e
lisafbe <lisafbe@s4pc29.eisc.univalle.edu.co>
2014-04-28 08:54:54 -0500

    Cambio variable

---
+++ Taller_Git/src/taller_git/Taller_Git.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_git;
+
+/**
+ *
+ * @author lisafbe
+ */
+public class Taller_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    private boolean variable1;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
