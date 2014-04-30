2c1e6302ddec8624c06e26a542bd012ae6b92682
Yalile Bermudes <Yalile Bermudes@Yalile>
2014-04-29 21:17:31 -0500

    Clase_Ensayo agregada

---
+++ Taller_Git/src/taller_git/Clase_Ensayo.java
@@ -0,0 +1,16 @@
+
+
+package taller_git;
+
+/**
+ *
+ * @author Yalile Bermudes
+ */
+public class Clase_Ensayo {
+    
+    int contar;
+    
+    public Clase_Ensayo(){
+        contar=0;
+    }
+}
