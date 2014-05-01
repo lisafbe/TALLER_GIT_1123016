6667d3ff21f44b4b9489ae9fd6ba7cb5bf6e43f9
joanna <joanna@Joanna>
2014-04-30 22:04:57 -0500

    ClasePruebaGit agregada efectivamente

---
+++ Taller_Git/src/taller_git/ClasePruebaGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package taller_git;
+
+/**
+ *
+ * @author joanna
+ */
+public class ClasePruebaGit {
+    
+    int varPrueba = 0;
+
+    public void setVarPrueba(int varPrueba) {
+        this.varPrueba = varPrueba;
+    }
+
+    public int getVarPrueba() {
+        return varPrueba;
+    }
+    
+}
