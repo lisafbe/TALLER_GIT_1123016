3365621c8226f2c153ef699a415ce1c8be228bfc
lady <lady@Toshiba-Laptop>
2014-04-30 20:49:29 -0500

    Agregar ClasePrueba

---
+++ Taller_Git/src/taller_git/ClasePrueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_git;
+
+/**
+ *
+ * @author leidy carvajal ortiz
+ */
+public class ClasePrueba {
+       int suma=0;
+
+    
+    public int calcularSuma(int a,int b){
+        suma=a+b;
+    return suma;
+    }
+}
